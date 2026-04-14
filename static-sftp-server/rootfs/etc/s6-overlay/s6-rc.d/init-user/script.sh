245eef5f399895e1af596569f29027e9172a183b
Lukino2000 <lukino2000@gmail.com>
2026-04-14 22:33:53 +0200

    fix key permissions

--- static-sftp-server/rootfs/etc/s6-overlay/s6-rc.d/init-user/script.sh
+++ static-sftp-server/rootfs/etc/s6-overlay/s6-rc.d/init-user/script.sh
@@ -3,25 +3,25 @@
 USER=${SFTP_USER:-sftpuser}
 PASS=${SFTP_PASS:-password}
 
-# Gestione Utente
 if ! id -u "$USER" >/dev/null 2>&1; then
     adduser -D -h /data "$USER"
 fi
 echo "$USER:$PASS" | chpasswd
 
-# Gestione Cartelle e Permessi
 mkdir -p /data/www /config
 chown root:root /data
 chmod 755 /data
 chown "$USER":"$USER" /data/www
 
-# Gestione Chiavi SSH Persistenti
 if [ ! -f /config/ssh_host_rsa_key ]; then
     ssh-keygen -A
     mv /etc/ssh/ssh_host_* /config/
 fi
 
-# Link simbolici per far trovare le chiavi a sshd nel percorso standard
 ln -sf /config/ssh_host_* /etc/ssh/
+
+chmod 600 /config/ssh_host_*_key
+chmod 644 /config/ssh_host_*_key.pub
+chown root:root /config/ssh_host_*
 
 exit 0
